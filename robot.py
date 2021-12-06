c24ce1d071d5b3411ffda585d6dafbb6b1359753
Andrew <34665271+Andrew-Sills@users.noreply.github.com>
2021-12-06 12:32:31 -0500

    Update robot.py
    initial trial for arcade drive

--- robot.py
+++ robot.py
@@ -39,15 +39,23 @@ def teleopPeriodic(self):
 	print("The drive X value is: ", self.controller.getX(self.controller.Hand.kLeftHand))
 	print("The drive Y value is: ", self.controller.getY(self.controller.Hand.kLeftHand))
 	print("The gyro Yaw value is: ", self.gyro.getYaw())
-	self.sd.putValue("Gyro Yaw", self.gyro.getYaw())
-	self.drive.driveCartesian(
-		self.controller.getX(self.controller.Hand.kLeftHand),
-		self.controller.getY(self.controller.Hand.kLeftHand),
-		self.controller.getY(self.controller.Hand.kRightHand), 0)
+# 	self.sd.putValue("Gyro Yaw", self.gyro.getYaw())
+# 	self.drive.driveCartesian(
+# 		self.controller.getX(self.controller.Hand.kLeftHand),
+# 		self.controller.getY(self.controller.Hand.kLeftHand),
+# 		self.controller.getY(self.controller.Hand.kRightHand), 0)
 	isAPressed = self.controller.getAButton()
 	isBPressed = self.controller.getBButton()
 	isXPressed = self.controller.getXButton()
 	isYPressed = self.controller.getYButton()
+	if(isAPressed):
+		self.front_left_motor.set(0.5)
+	if(isBPressed):
+		self.rear_left_motor.set(0.5)
+	if(isXPressed):
+		self.front_right_motor.set(0.5)
+	if(isYPressed):
+		self.rear_right_motor.set(0.5)
 
 if __name__ == "__main__":
 	wpilib.run(MyRobot)
