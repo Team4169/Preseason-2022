c64fbc83b85669a114e423186604f6ed1486b8de
Skyler <wierniks22@lsrhs.net>
2021-12-14 16:10:36 -0500

    Tab issue

--- robot.py
+++ robot.py
@@ -9,13 +9,13 @@ class MyRobot(wpilib.TimedRobot):
 	def robotInit(self):
 		self.front_left_motor = ctre.WPI_TalonSRX(constants["frontLeftPort"])
 		self.rear_left_motor = ctre.WPI_VictorSPX(constants["rearLeftPort"])
-  		self.rear_left_motor.setInverted(True)
-  		self.left = wpilib.SpeedControllerGroup(self.front_left_motor, self.rear_left_motor)
+		self.rear_left_motor.setInverted(True)
+		self.left = wpilib.SpeedControllerGroup(self.front_left_motor, self.rear_left_motor)
 		
-  		self.front_right_motor = ctre.WPI_TalonSRX(constants["frontRightPort"])
-  		self.rear_right_motor = ctre.WPI_VictorSPX(constants["rearRightPort"])
-  		self.rear_right_motor.setInverted(True)
-  		self.right = wpilib.SpeedControllerGroup(self.front_right_motor, self.rear_right_motor)
+		self.front_right_motor = ctre.WPI_TalonSRX(constants["frontRightPort"])
+		self.rear_right_motor = ctre.WPI_VictorSPX(constants["rearRightPort"])
+		self.rear_right_motor.setInverted(True)
+		self.right = wpilib.SpeedControllerGroup(self.front_right_motor, self.rear_right_motor)
 		
 		self.drive = wpilib.drive.DifferentialDrive(
 			self.right,
