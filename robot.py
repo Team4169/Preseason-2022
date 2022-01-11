26c6147ab90d77b9b769e0404250e012c2a18e8e
MrButtner <wierniks22@lsrhs.net>
2022-01-11 17:08:42 -0500

    changing constants for actual motor device IDs

--- robot.py
+++ robot.py
@@ -3,27 +3,24 @@ import wpilib
 import wpilib.drive
 import ctre
 from constants import constants
-from networktables import NetworkTables
-import navx
 
 class MyRobot(wpilib.TimedRobot):
-
 
     def robotInit(self):
         """Robot initialization function"""
 
         # object that handles basic drive operations
-     	  self.front_left_motor = ctre.WPI_TalonSRX(constants["frontLeftPort"])
+        self.front_left_motor = ctre.WPI_TalonSRX(constants["frontLeftPort"])
         self.rear_left_motor = ctre.WPI_VictorSPX(constants["rearLeftPort"])
         self.left = wpilib.SpeedControllerGroup(
             self.front_left_motor, self.rear_left_motor)
 
-        self.front_right_motor = ctre.WPI_TalonSRX(constants["frontRightPort"])
-        self.rear_right_motor = ctre.WPI_VictorSPX(constants["rearRightPort"])
+        self.front_right_motor = ctre.WPI_VictorSPX(constants["frontRightPort"])
+        self.rear_right_motor = ctre.WPI_TalonSRX(constants["rearRightPort"])
         self.right = wpilib.SpeedControllerGroup(
             self.front_right_motor, self.rear_right_motor)
 
-	  self.drive = wpilib.drive.DifferentialDrive(
+        self.drive = wpilib.drive.DifferentialDrive(
             self.right,
             self.left
         )
@@ -32,14 +29,14 @@ class MyRobot(wpilib.TimedRobot):
         self.controller = wpilib.XboxController(0)
 
     def teleopInit(self):
-        self.myRobot.setSafetyEnabled(True)
+        #self.myRobot.setSafetyEnabled(True)
+        pass
 
     def teleopPeriodic(self):
-	  self.myRobot.tankDrive(
-	      self.controller.getY(self.controller.Hand.kLeftHand) * -1,
+        self.drive.tankDrive(
+            self.controller.getY(self.controller.Hand.kLeftHand) * -1,
             self.controller.getY(self.controller.Hand.kRightHand) * -1)
-	  )
-           
+
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
