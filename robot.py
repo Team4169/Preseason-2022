3079ef6cc1e687d2d6be07df544d4c6f8411668e
Noah Büttner <noahbuttner@gmail.com>
2022-02-08 22:26:04 -0500

    adding to preseason repo from the 2022 for testing tomorrow

--- robot.py
+++ robot.py
@@ -5,19 +5,19 @@ from constants import constants
 from networktables import NetworkTables
 import navx
 
+
+
 class MyRobot(wpilib.TimedRobot):
     """This is a demo program showing how to use Gyro control with the
     DifferentialDrive class."""
 
     def robotInit(self):
         """Robot initialization function"""
-        self.angleSetpoint = 0.0
-
-        #smart dashboard
+        # smart dashboard
         self.sd = NetworkTables.getTable("SmartDashboard")
         # gyro calibration constant, may need to be adjusted
         # gyro value of 360 is set to correspond to one full revolution
-        
+
         # self.voltsPerDegreePerSecond = 0.0128
 
         self.front_left_motor = ctre.WPI_TalonSRX(constants["frontLeftPort"])
@@ -40,7 +40,9 @@ class MyRobot(wpilib.TimedRobot):
         self.timer = wpilib.Timer()
         self.sd = NetworkTables.getTable("SmartDashboard")
         self.gyro = navx.AHRS.create_i2c(wpilib.I2C.Port.kMXP)
-        self.left_tpf = -924
+        #change to negative if necessary
+        self.tpf = 924
+        self.max_speed = 0.4
 
     def teleopInit(self):
         """
@@ -49,39 +51,122 @@ class MyRobot(wpilib.TimedRobot):
         self.gyro.reset()
         self.front_left_motor.setSelectedSensorPosition(0, 0, 10)
         # self.gyro.setSensitivity(
-            # self.voltsPerDegreePerSecond
+        # self.voltsPerDegreePerSecond
         # )  # calibrates gyro values to equal degrees
+        self.pGain = self.sd.getValue("PGain", 0.032)
+        self.kP = self.sd.getValue("kP", 0.05)
+        self.steps = [{
+            "Step_Type": "Straight", #Step_Type says whether we will be driving forward, or turning in this step
+            "Distance": 3, # How far we need to move forward in this step
+            "Angle": 0, # What angle we need to turn to in this step
+            "Threshold_Value": .1, # To complete the step, we need to be within the threshold feet of the target distance
+            "Threshold_Time": 1, # Once we are in the threshold for this amount of time, we move to the next step
+        },{
+            "Step_Type": "Turn",
+            "Distance": 0,
+            "Angle": 90,
+            "Threshold_Value": 1,
+            "Threshold_Time": 1,
+        },{
+            "Step_Type": "Straight",
+            "Distance": 3,
+            "Angle": 90,
+            "Threshold_Value": .1,
+            "Threshold_Time": 1,
+        },{
+            "Step_Type": "Turn",
+            "Distance": 0,
+            "Angle": 180,
+            "Threshold_Value": 1,
+            "Threshold_Time": 1,
+        },
+        ]
+        self.current_step_index = 0
+        self.current_step = self.steps[self.current_step_index]
+        self.in_threshold_time = 0
+        self.in_threshold_start_time = 0
+        self.in_threshold = False
+        self.steps_complete = False
 
     def teleopPeriodic(self):
-        """
-        Sets the gyro sensitivity and drives the robot when the joystick is pushed. The
-        motor speed is set from the joystick while the RobotDrive turning value is assigned
-        from the error between the setpoint and the gyro angle.
-        """
-        self.pGain = self.sd.getValue("PGain", 0.032)
-        self.dist = self.sd.getValue("Drive Dist", 5) * self.left_tpf
-        self.error = self.dist - self.front_left_motor.getSelectedSensorPosition()
-        self.kP = self.sd.getValue("kP", 0.05)
         self.isAPressed = self.controller.getAButton()
         self.isBPressed = self.controller.getBButton()
         self.isXPressed = self.controller.getXButton()
         self.isYPressed = self.controller.getYButton()
-        turningValue = (self.angleSetpoint - self.gyro.getYaw()) * self.pGain
-        # speed = self.controller.getY(self.controller.Hand.kLeftHand)
-        speed = self.error * self.kP
-        self.sd.putValue("Speed", speed)
-        self.sd.putValue("Left Encoder Value", self.front_left_motor.getSelectedSensorPosition())
-        if self.isBPressed and abs(self.front_left_motor.getSelectedSensorPosition()) < abs(self.dist):
-            self.drive.arcadeDrive(speed, turningValue)
+        if self.isXPressed: #Use X to cancel program
+            self.steps_complete = True
+            return
+        if self.steps_complete:
+            return
+        if self.in_threshold:
+            self.in_threshold_time = self.timer.get() - self.in_threshold_start_time
         else:
-            self.drive.arcadeDrive(self.controller.getY(self.controller.Hand.kLeftHand), self.controller.getY(self.controller.Hand.kRightHand))
-        if self.isYPressed:
-            self.gyro.reset()
+            self.in_threshold_time = 0
+        #Determine if in threshold
+        if self.current_step['Step_Type'] == "Straight":
+            current_in_threshold = abs(self.goal_tick_dist - self.front_left_motor.getSelectedSensorPosition()) < self.current_step[
+                'Threshold_Value']
+        elif self.current_step['Step_Type'] == "Turn":
+            current_in_threshold = self.goal_angle - self.gyro.getYaw() < self.current_step['Threshold_Value']
+
+        if current_in_threshold:
+            if self.in_threshold == False:
+                #Just entered the threshold
+                self.in_threshold = True
+                self.in_thershold_start_time = self.timer.get()
+            if self.in_threshold_time > self.current_step['Threshold_Time']:
+                #Been in threshold for a good amount of time, move to next step
+                self.current_step_index += 1
+                if self.current_step_index == len(self.steps):
+                    self.steps_complete = True
+                    return
+                else:
+                    self.current_step = self.steps[self.current_step_index]
+                self.front_left_motor.setSelectedSensorPosition(0, 0, 10)
+                self.in_threshold = False
+        else:
+            self.in_threshold = False
+
+        #Setting Speed
+        if self.current_step['Step_Type'] == "Straight":
+            self.goal_tick_dist = self.current_step['Distance'] * self.tpf
+            self.distance_error = self.goal_tick_dist - self.front_left_motor.getSelectedSensorPosition()
+            self.speed = self.distance_error * self.kP
+            if self.speed > self.max_speed:
+                self.speed = self.max_speed
+            if self.speed < self.max_speed * -1:
+                self.speed = -1 * self.max_speed
+        elif self.current_step['Step_Type'] == "Turn":
+            self.speed = 0 # we don't want to move forward when turning (turn in place)
+
+        #Setting Angle - this drives helps drive
+        self.goal_angle = self.current_step['Angle']
+        turningValue = (self.goal_angle - self.gyro.getYaw()) * self.pGain
+
+        #move the robot
+        self.drive.arcadeDrive(self.speed, turningValue)
+
+        #print debug values
+
+        self.sd.putValue("IsBPressed", self.isBPressed)
+        self.sd.putValue("current_step_index", self.current_step_index)
+        self.sd.putValue("Step Type", self.current_step['Step_Type'])
+
+        self.sd.putValue("Current Time", self.timer.get())
+        self.sd.putValue("in_threshold_start_time", self.in_threshold_start_time)
+        self.sd.putValue("in_threshold", self.in_threshold)
+        self.sd.putValue("in_threshold_time", self.in_threshold_time)
+        self.sd.putValue("Steps Complete?", self.steps_complete)
+
+        self.sd.putValue("Goal Angle", self.goal_angle)
         self.sd.putValue("Gyro Yaw", self.gyro.getYaw())
         self.sd.putValue("Turning Value", turningValue)
         self.sd.putValue("PGain", self.pGain)
-        self.sd.putValue("Straight", self.isBPressed)
-        self.sd.putValue("error", self.error)
+
+        self.sd.putValue("goal_tick_dist", self.goal_tick_dist)
+        self.sd.putValue("Left Encoder Value", self.front_left_motor.getSelectedSensorPosition())
+        self.sd.putValue("Distance Error", self.distance_error)
+        self.sd.putValue("Speed", self.speed)
 
 
 if __name__ == "__main__":
