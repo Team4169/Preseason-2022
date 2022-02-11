661027151368bd00cad5c1abc28a0dde9dced932
Noah Büttner <noahbuttner@gmail.com>
2022-02-11 10:48:31 -0500

    making so path is imported from file

--- robot.py
+++ robot.py
@@ -78,57 +78,8 @@ class MyRobot(wpilib.TimedRobot):
         """
         self.gyro.reset()
         self.front_left_motor.setSelectedSensorPosition(0, 0, 10)
-
-        self.steps = [{
-            "Step_Type": "Straight", #Step_Type says whether we will be driving forward, or turning in this step
-            "Distance": 5, # How far we need to move forward in this step
-            "Angle": 0, # What angle we need to turn to in this step
-            "Threshold_Value": .1, # To complete the step, we need to be within the threshold feet of the target distance
-            "Threshold_Time": 1, # Once we are in the threshold for this amount of time, we move to the next step
-        },{ #top left turn
-            "Step_Type": "Turn",
-            "Distance": 0,
-            "Angle": 90,
-            "Threshold_Value": 5,
-            "Threshold_Time": 1, 
-        },{
-            "Step_Type": "Straight",
-            "Distance": 5,
-            "Angle": 90,
-            "Threshold_Value": .1,
-            "Threshold_Time": 1,
-        },{# top right turn
-            "Step_Type": "Turn",
-            "Distance": 0,
-            "Angle": 180,
-            "Threshold_Value": 5,
-            "Threshold_Time": 1, 
-        },{
-            "Step_Type": "Straight",
-            "Distance": 5,
-            "Angle": 180,
-            "Threshold_Value": .1,
-            "Threshold_Time": 1,
-        },{#bottom right turn
-            "Step_Type": "Turn",
-            "Distance": 0,
-            "Angle": -90,
-            "Threshold_Value": 5,
-            "Threshold_Time": 1, 
-        },{
-            "Step_Type": "Straight",
-            "Distance": 5,
-            "Angle": -90,
-            "Threshold_Value": .1,
-            "Threshold_Time": 1,
-        },{#bottome left turn back to orientation at the start
-            "Step_Type": "Turn",
-            "Distance": 0,
-            "Angle": 0,
-            "Threshold_Value": 5,
-            "Threshold_Time": 1, 
-        },
-        ]
+        from autos.square import auto
+        self.steps = auto
         self.current_step_index = 0
         self.current_step = self.steps[self.current_step_index]
         self.in_threshold_time = 0
